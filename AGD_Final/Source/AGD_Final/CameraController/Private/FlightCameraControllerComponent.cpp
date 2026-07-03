baf7b4ac60d3b2a377dd4f7ca26eccd2ce445ffd
Çınar Güvençli <cinarguvencli@hotmail.com>
2026-07-03 17:32:31 +0300

    feat: added CameraController
    Camera gets far behind once the dragon accelarates and slows down upon
    deaccelaration

---
+++ AGD_Final/Source/AGD_Final/CameraController/Private/FlightCameraControllerComponent.cpp
@@ -0,0 +1,40 @@
+﻿#include "../Public/FlightCameraControllerComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UFlightCameraControllerComponent::UFlightCameraControllerComponent(): ParentSpringArm(nullptr), OwnerCharacter(nullptr)
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UFlightCameraControllerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    ParentSpringArm = Cast<USpringArmComponent>(GetAttachParent());
+    OwnerCharacter = Cast<ACharacter>(GetOwner());
+}
+
+void UFlightCameraControllerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (!OwnerCharacter || !OwnerCharacter->GetCharacterMovement() || !ParentSpringArm)
+    {
+        return; 
+    }
+
+    float CurrentVelocity = OwnerCharacter->GetVelocity().Size();
+    float MaxVelocity = OwnerCharacter->GetCharacterMovement()->MaxFlySpeed;
+    
+    float TargetSpeedAlpha = (MaxVelocity > 0.0f) ? FMath::Clamp(CurrentVelocity / MaxVelocity, 0.0f, 1.0f) : 0.0f;
+
+    CurrentSpeedAlpha = FMath::FInterpTo(CurrentSpeedAlpha, TargetSpeedAlpha, DeltaTime, CameraInterpolationSpeed);
+
+    float TargetFOV = FMath::Lerp(SlowFOV, FastFOV, CurrentSpeedAlpha);
+    SetFieldOfView(TargetFOV);
+
+    float TargetArmLength = FMath::Lerp(SlowCameraDistance, FastCameraDistance, CurrentSpeedAlpha);
+    ParentSpringArm->TargetArmLength = TargetArmLength;
+}
---
+++ AGD_Final/Source/AGD_Final/CameraController/Public/FlightCameraControllerComponent.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "FlightCameraControllerComponent.generated.h"
+
+class USpringArmComponent;
+class ACharacter;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AGD_FINAL_API UFlightCameraControllerComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFlightCameraControllerComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Flight Camera")
+	float SlowCameraDistance = 400.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Flight Camera")
+	float FastCameraDistance = 600.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Flight Camera")
+	float SlowFOV = 90.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Flight Camera")
+	float FastFOV = 110.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Flight Camera")
+	float CameraInterpolationSpeed = .5f;
+
+private:
+	USpringArmComponent* ParentSpringArm;
+	ACharacter* OwnerCharacter;
+
+	float CurrentSpeedAlpha = 0.0f;
+};
